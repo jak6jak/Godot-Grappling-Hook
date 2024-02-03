b92b6ed47ddab591b7447ec86f6985105479c817
jacobEdie <jacobedie1@gmail.com>
2024-02-03 18:17:53 -0500

    Added grappling!!!

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,10 +3,20 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SWING_SPEED = 5
+const MAX_FALLING_WALK_SPEED =6
 @onready var Head = $"Head"
+@onready var camera = $"Head/Camera3D"
+@onready var prev_pos = transform.origin
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var grappling = false
+var grappling: bool = false
+var grapple_anchor: Vector3
+@onready var GroundCheckRay = $GroundCheckRay
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
 
@@ -14,10 +24,19 @@ func _input(event):
 		grappling = false
 		
 
+func verletIntegration(prev_pos: Vector3, forces: Vector3, delta: float):
+	var accel = forces # 1.0 is the mass of the player
+	return 2 * transform.origin - prev_pos + accel * pow(delta,2)
+
+func constrain_rope(pos : Vector3, max_rope_length: float):
+	var rope_vector = pos - grapple_anchor
+	if rope_vector.length()> max_rope_length:
+		rope_vector = rope_vector.normalized() * max_rope_length
+		return grapple_anchor + rope_vector
+	return pos
+
+
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -31,22 +50,45 @@ func _physics_process(delta):
 	#Test if grapple shot hits
 	if Input.is_action_just_pressed("grapple") and not grappling:
 		#Check if grapple shot hit an object
-		if true:
-			grappling = true
-			pass
-		else:
-			pass
-
+		var viewPortSize = get_viewport().size
+		var space_state = get_world_3d().direct_space_state
+		var from = camera.project_ray_origin(Vector2(viewPortSize.x/2,viewPortSize.y/2))
+		var to = from + camera.project_ray_normal(Vector2(viewPortSize.x/2,viewPortSize.y/2)) * 100
+		var query = PhysicsRayQueryParameters3D.create(from,to,1)
+		var RayCast= space_state.intersect_ray(query)
 		
-	
+		if RayCast.size() > 0:
+			grappling = true
+			grapple_anchor = RayCast.position
+
 	#move player as grappling
 	if grappling:
-		pass
+		var total_forces = direction * SWING_SPEED
+		if not GroundCheckRay.is_colliding():
+			total_forces += Vector3(0,-gravity,0)
+		var new_pos = verletIntegration(prev_pos,total_forces,delta)
+		var rope_length = transform.origin.distance_to(grapple_anchor)
+		new_pos = constrain_rope(new_pos, rope_length)
+		velocity = (new_pos - transform.origin) / delta
+		
+		prev_pos = transform.origin
+		move_and_slide()
 	else:
 		if not is_on_floor():
-			pass
+			velocity.y -= gravity * delta
+			var falling_movement = direction * SPEED
+			falling_movement += Vector3.UP * velocity.y
+			var deltaVelocity = falling_movement - velocity
+			var accel = deltaVelocity
+			var limit_velocity = 2 if accel.dot(velocity) > 0 else 1
+			if accel.length() > limit_velocity:
+				accel = accel.normalized() * limit_velocity
+			velocity += deltaVelocity
+			prev_pos = transform.origin
+			move_and_slide()
+			
 		else:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
-
-	move_and_slide()
+			prev_pos = transform.origin
+			move_and_slide()
