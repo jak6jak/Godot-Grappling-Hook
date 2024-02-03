374040ec3f97e10011ac9b8f5ff245c4468b8375
jacobEdie <jacobedie1@gmail.com>
2024-02-03 16:52:07 -0500

    Added grappling state

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,7 +6,13 @@ const JUMP_VELOCITY = 4.5
 @onready var Head = $"Head"
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var grappling = false
 
+func _input(event):
+
+	if event.is_action_released("grapple"):
+		grappling = false
+		
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,11 +27,26 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (Head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	
+	#Test if grapple shot hits
+	if Input.is_action_just_pressed("grapple") and not grappling:
+		#Check if grapple shot hit an object
+		if true:
+			grappling = true
+			pass
+		else:
+			pass
+
+		
+	
+	#move player as grappling
+	if grappling:
+		pass
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if not is_on_floor():
+			pass
+		else:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 
 	move_and_slide()
